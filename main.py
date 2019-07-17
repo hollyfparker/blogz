71d66e561822697474088d3fa2b28b7af735e715
Holly Parker <hollyfparker@gmail.com>
2019-07-17 11:46:50 -0500

    commit

--- main.py
+++ main.py
@@ -10,27 +10,40 @@ db = SQLAlchemy(app)
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)
 
-    def __init__(self, name):
+    def __init__(self, name, body):
         self.name = name 
-
-
-blogs = []
+        self.body = body
 
 @app.route('/')
 
-def blog():
-    return render_template('blog.html')
+def index:
+    
+    return redirect('/blog')
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/blog')
+
+def blog():
+    blog_id = request.ergs.get('id')
+
+    if blog_id == None:
+        posts = Blog.query.all()
+        return render_template('blog.html', posts=posts, title='Build-a-Blog')
+
+    else:
+        post = Blog.query.get('blog_id')
+        return render_template('newpost.html', post=post)
+
+@app.route('/newpost', methods=['POST', 'GET'])
 def new_blog():
     name = request.form['title']
     new_blog = Blog(name)
     db.session.add(new_blog)
     db.session.commit()
 
-    return render_template('newpost.html')
+    return render_template('newpost.html', title='title', text='text')
 
 
 app.run()
