7c5b8f815f8311684bb9dc638d7515416f09aff9
Holly Parker <hollyfparker@gmail.com>
2019-07-25 20:31:42 -0500

    again

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
-    #owner = db.Column(db.String)
+    username = db.Column(db.String(120))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, username):
         self.title = title
         self.body = body
-        #self.owner = owner
+        self.username = username 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
